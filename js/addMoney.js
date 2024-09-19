66bc2422367b67e8840be25488c9fda991df4925
Habibur Rahman <habibur88454@gmail.com>
2024-09-20 02:34:09 +0600

    trans added dynamically using template string

--- js/addMoney.js
+++ js/addMoney.js
@@ -9,6 +9,13 @@ document.getElementById('btn-add-money').addEventListener('click', function (e) 
         const prevBalance = getTextFieldValueById('prev-balance')
         const newBalance = prevBalance + addMoney;
         document.getElementById('prev-balance').innerText = newBalance;
+
+        // added trans history
+        const transContainer = document.getElementById('trans-container');
+        const p = document.createElement('p');
+        p.classList.add('bg-teal-700', 'font-bold', 'text-white', 'py-3')
+        p.innerText = `Add amount: ${addMoney} New balance: ${newBalance}`
+        transContainer.appendChild(p)
     }
     else {
         alert('Please try again to add money')
--- js/cashOut.js
+++ js/cashOut.js
@@ -5,8 +5,22 @@ document.getElementById('btn-cash-out').addEventListener('click', function (e) {
 
     if (pinNumber === 1234) {
         const prevBalance = getTextFieldValueById('prev-balance');
+        
+        if(amount > prevBalance){
+            alert('Ato tk nai re')
+            return
+        }
+
         const newBalance = prevBalance - amount;
         document.getElementById('prev-balance').innerText = newBalance;
+
+        // trans cash out
+        const transContainer = document.getElementById('trans-section');
+        const div = document.createElement('div');
+        div.innerHTML = `<div class="bg-sky-700 text-white py-4">
+                            <p>Cash out: ${amount} New balance: ${newBalance}</p>
+                        </div>`
+        transContainer.appendChild(div)
     }
     else {
         alert('Wrong number or password')
