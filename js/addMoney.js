859a8adf279d1a72aa8c5b2177b6093a0c7a82ac
Habibur Rahman <habibur88454@gmail.com>
2024-09-19 20:17:08 +0600

    get input field value by id using func

--- js/addMoney.js
+++ js/addMoney.js
@@ -1,4 +1,7 @@
 
 document.getElementById('btn-add-money').addEventListener('click', function (e) {
     e.preventDefault();
+
+    const addMoney = getInputFieldValueById('field-new-amount');
+    console.log('input', addMoney)
 })
